8f8f2ad26464dd0bff9fc50414a8da12e187b2f4
David Reynolds <dbreynol@umich.edu>
2017-10-30 09:55:29 -0400

    loops in c++

--- irwls.R
+++ irwls.R
@@ -45,7 +45,7 @@ irls <- function (f, b, b1, b1inv, b2, init, tol = 1e-6) {
 }
 
 
-# implementation of IRLS where both loops through the design matrix are implemented in C ++
+# Faster IRLS function where both loops through the design matrix are implemented in C ++
 library(Rcpp)
 sourceCpp("suff_stats.cpp")
 sourceCpp("eta_update.cpp")
